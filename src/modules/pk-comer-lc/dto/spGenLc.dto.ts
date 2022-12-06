2a0e512f1203d4c5061397baab67596cb316f429
Edwin Pajuelo <edwin.apd.1198@gmail.com>
2022-12-06 14:41:21 -0500

    develop

--- src/modules/pk-comer-lc/dto/spGenLc.dto.ts
+++ src/modules/pk-comer-lc/dto/spGenLc.dto.ts
@@ -7,15 +7,35 @@ import { Message } from "src/shared/message";
 
 export class SpGenLcDto {
     //-----------------------------------------------------------------------------------------------------------------
-    P_ID_LOTE: string
+    @IsNumber({}, { message: Message.NUMBER('$property') })
+    @IsInt({ message: Message.ISINT('$property') })
+    @Max(9999999999, { message: Message.MAX('$property', '$constraint1') })
+    @ApiProperty({ example: 'INDENTIFICADOR DEL ID_LOTE' })
+    P_ID_LOTE: number
     //-----------------------------------------------------------------------------------------------------------------
-    P_ID_CLIENTE: string
+    @IsNumber({}, { message: Message.NUMBER('$property') })
+    @IsInt({ message: Message.ISINT('$property') })
+    @Max(9999999, { message: Message.MAX('$property', '$constraint1') })
+    @ApiProperty({ example: 'INDENTIFICADOR DEL ID_CLIENTE' })
+    P_ID_CLIENTE: number
     //-----------------------------------------------------------------------------------------------------------------
+    @IsString({ message: Message.STRING('$property') })
+    @MaxLength(20, { message: Message.MAXLENGTH('$property', '$constraint1') })
+    @ApiProperty({ example: 'IDENTIFICADOR DEL PARAMETRO DE TIPO DE LC' })
     P_PARAMETRO: string
     //-----------------------------------------------------------------------------------------------------------------
-    P_MONTO: string
+    @IsNumber({}, { message: Message.NUMBER('$property') })
+    @Max(99999999999999999999, { message: Message.MAX('$property', '$constraint1') })
+    @ApiProperty({ example: 'IDENTIFICADOR DE MONTO DE LC PARA EL CASO LAS QUE SE GENEREN POR EL PORTAL (PAGO PARCIAL)' })
+    P_MONTO: number
     //-----------------------------------------------------------------------------------------------------------------
-    P_IND_MOV: number
+    @IsString({ message: Message.STRING('$property') })
+    @MaxLength(20, { message: Message.MAXLENGTH('$property', '$constraint1') })
+    @ApiProperty({ example: 'IDENTIFICADOR DE MOVIMIENTO  CHEQUE O TRANSFERENCIA' })
+    P_IND_MOV: string
     //-----------------------------------------------------------------------------------------------------------------
-    P_FECVIGENCIA: string
+    @Type(() => Date)
+    @IsDate({ message: Message.ISDATE('$property') })
+    @ApiProperty({ example: 'IDENTIFICADOR DE FECHA PARA EL CASO DE LOS PAGOS  PARCIALES' })
+    P_FECVIGENCIA: Date
 }
