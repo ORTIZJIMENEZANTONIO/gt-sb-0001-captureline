d340e2d6db0e14b9766598112a449845442aa208
Edwin Pajuelo <edwin.apd.1198@gmail.com>
2022-12-14 18:20:48 -0500

    modulos

--- src/modules/enel-ms/enel-ms.controller.ts
+++ src/modules/enel-ms/enel-ms.controller.ts
@@ -3,6 +3,8 @@ import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { EnelMsService } from './enel-ms.service';
 import { ObtenLcTerceroDto } from './dto/obtenLcTercero.dto';
 import { ComerCalcLeneaDto } from './dto/comerCalcLenea.dto';
+import { ModificaLcDto } from './dto/modificaLc.dto';
+import { ActualizalccasDto } from './dto/actualizalccas.dto';
 
 @Controller('enel-ms')
 @ApiTags('enel-ms')
@@ -21,5 +23,26 @@ export class EnelMsController {
     @Post('obten-lc-tercero')
     async obtenLcTercero(@Body() dataBody: ObtenLcTerceroDto) {
         return this.service.obtenLcTercero(dataBody)
+    }
+    //---------------------------------------------------------------------------------------------
+    @ApiOperation({ summary: 'modifica-lc-gen' })
+    @ApiBody({ type: ModificaLcDto })
+    @Post('modifica-lc-gen')
+    async modificaLcGen(@Body() dataBody: ModificaLcDto) {
+        return this.service.modificaLcGen(dataBody)
+    }
+    //---------------------------------------------------------------------------------------------
+    @ApiOperation({ summary: 'modifica-lc-cas' })
+    @ApiBody({ type: ModificaLcDto })
+    @Post('modifica-lc-cas')
+    async modificaLcCas(@Body() dataBody: ModificaLcDto) {
+        return this.service.modificaLcCas(dataBody)
+    }
+    //---------------------------------------------------------------------------------------------
+    @ApiOperation({ summary: 'actualizalccas' })
+    @ApiBody({ type: ModificaLcDto })
+    @Post('actualizalccas')
+    async actualizalccas(@Body() dataBody: ActualizalccasDto) {
+        return this.service.actualizalccas(dataBody)
     }
 }
